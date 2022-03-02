2ca42256f30d361eebcc9f8cc538133a79ce8a6b
szegediviktor <szeg.viktor@gmail.com>
2022-03-02 15:44:08 +0100

    add comments (hun)

--- fetch-in-react/src/components/BasicFetch.jsx
+++ fetch-in-react/src/components/BasicFetch.jsx
@@ -3,21 +3,27 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
-const url = "https://dog.ceo/api/breeds/image/random";
-
+// Létrehozom a függvényt amely a fetchet hajtja végre.
 const fetchFromApi = () => {
-    const promise = fetch(url).then((res) => {
-        if (res.ok) {
-            return res.json();
+    // fetchelem az url-t, majd .then callbackben megnézem, hogy a res.ok létezik-e (sikeres volt-e a fetch), amennyiben igen,  visszatérek a res json adattá alakított verziójával. (Dzsézonizálom).
+    // Ha nem volt sikeres akkor errort dobok.
+    const promise = fetch("https://dog.ceo/api/breeds/image/random").then(
+        (res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            throw res;
         }
-        throw res;
-    });
+    );
+    // visszatérek a promise változóval amelyben a res.json() eredménye található
     return promise;
 };
 
 const BasicFetch = () => {
+    // Statet hozok létre, amelyben tárolom majd az adatokat.
     const [data, setData] = useState("");
 
+    // UseEffect egyszer fut le az első render előtt ([] miatt). Törzsében meghívom a fetchet elvégző függvényünket amely így visszaad egy promise-t amiből már csak egy újabb .then() segítségével adatok nyerek ki, amelyet átadok a szetter függvénynek. Így a setData beállítja a data-t.
     useEffect(() => {
         fetchFromApi().then((data) => {
             setData(data);
